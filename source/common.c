79cdf401913cffb47bc02dd21169c418d2db80d5
perhuepenbecker <perhuepenbecker@googlemail.com>
2024-11-11 11:04:30 +0100

    work on dataset init - unfinished

--- source/common.c
+++ source/common.c
@@ -1,3 +1,69 @@
 //
 // Created by Per Hüpenbecker on 10.11.24.
 //
+
+#include "common.h"
+
+void dataset_t_init(dataset_t* dataset, size_t x_dimensions){
+    dataset -> data_points = malloc(sizeof(data_point_t) * INITIAL_CAPACITY);
+    dataset -> data_points_capacity = INITIAL_CAPACITY;
+    dataset -> data_points_count = 0;
+    dataset -> x_dimensions = x_dimensions;
+    dataset -> data_pool = malloc(sizeof(double) * INITIAL_CAPACITY * (x_dimensions + 1));
+
+    if(dataset -> data_points == NULL || dataset -> data_pool == NULL){
+        fprintf(stderr, "Error: Initial memory allocation failed\n");
+        exit(1);
+    }
+}
+
+// Simple destructor. Frees all allocated memory. Here comes the approach of using centralized memory
+// allocation into play. This way we can safely free all memory with a single call to free for each
+// dynamic array.
+
+void dataset_destroy(dataset_t* dataset) {
+    free(dataset -> data_points);
+    free(dataset -> data_pool);
+}
+
+
+void dataset_push_data_point(dataset_t* dataset, double* x, size_t x_dimensions, double y) {
+    if (x_dimensions != dataset -> x_dimensions) {
+        fprintf(stderr, "Error: Dimensions of x do not match dataset dimensions\n");
+        return;
+    }
+
+    if(dataset -> data_points_capacity - dataset -> data_points_count == 0){
+        void* tmp = realloc(dataset -> data_points, sizeof(data_point_t) * GROWTH_FACTOR);
+        if(tmp == NULL){
+            fprintf(stderr, "Error: Memory allocation failed\n");
+            return;
+        }
+
+        for (size_t i = dataset->data_points_count; i < dataset->data_points_capacity; i++){
+            dataset->data_points[i].x = NULL;
+            dataset->data_points[i].y = NULL;
+        }
+
+        dataset -> data_points = tmp;
+
+        tmp = realloc(dataset -> data_pool, sizeof(double) * GROWTH_FACTOR * (x_dimensions + 1));
+        if(tmp == NULL){
+            fprintf(stderr, "Error: Memory allocation failed\n");
+            return;
+        }
+
+        dataset -> data_pool = tmp;
+        dataset -> data_points_capacity *= GROWTH_FACTOR;
+    }
+
+    for(int i = 0; i < x_dimensions; i++){
+        dataset -> data_pool[dataset -> data_points_count * (x_dimensions + 1) + i] = x[i];
+        dataset -> data_pool[dataset -> data_points_count * (x_dimensions + 1) + x_dimensions] = y;
+    }
+
+    dataset -> data_points[dataset -> data_points_count].x = &dataset -> data_pool[dataset -> data_points_count * (x_dimensions + 1)];
+    dataset -> data_points[dataset -> data_points_count].y = &dataset -> data_pool[dataset -> data_points_count * (x_dimensions + 1) + x_dimensions];
+    dataset -> data_points_count++;
+
+}
--- source/common.h
+++ source/common.h
@@ -7,48 +7,46 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include "stdio.h"
+
+#define GROWTH_FACTOR 2
+#define GROWTH_THRESHOLD 1<<20
+#define INITIAL_CAPACITY 10
 
 // Standard DataPoint struct for x and y values. The attributes x and y are defined as pointers to
 // allow for separate caching of the values.
 
 typedef struct {
     double* x;
-    size_t x_dimensions;
-    double y;
+    double* y;
 } data_point_t;
-
-// Data structure for pooling the read x values from the dataset. This is used to avoid multiple
-// memory allocations for each x value by keeping the possibility to allow multidimensional x values
-// at runtime without the need to change the underlying data structure or to rely on compile
-// time macros.
-
-typedef struct {
-    double* x_value_pool;
-    size_t x_value_pool_size;
-} x_value_pool_t;
 
 // Data structure for handling the dataset consisting of DataPoints + Owner of all allocated
 // memory for the DataPoints.
+// Complete x and y data is stored in the data_pool array. The DataPoints only store pointers to
+// the data_pool array and work as a logical grouping. I chose this approach to allow for leveraging
+// the cache more effectively and to reduce the number of memory allocations, especially if ever
+// working with large datasets. This is hopefully a trade-off between complexity and performance.
 
 typedef struct {
     data_point_t* data_points;
+    size_t data_points_capacity;
     size_t data_points_count;
-
+    size_t x_dimensions;
+    double* data_pool;
 } dataset_t;
 
+void dataset_t_init_from_csv(dataset_t* dataset, const char* file_path);
 
+void dataset_t_init(dataset_t* dataset, size_t x_dimensions);
 
-void dataset_t_init(dataset_t* dataset, size_t data_points_count){
-    dataset -> data_points = malloc(data_points_count * sizeof(data_point_t));
-    dataset -> data_points_count = data_points_count;
-}
+void dataset_push_data_point(dataset_t* dataset, double* x, size_t x_dimensions, double y);
 
-void dataset_destroy(dataset_t* dataset) {
-    free(dataset -> data_points);
-}
+void dataset_destroy(dataset_t* dataset);
 
 
 
 
 
-#endif //LINEARREGRESSION_COMMON_H
+
+#endif //LINEARREGRESSION_C_COMMON_H
