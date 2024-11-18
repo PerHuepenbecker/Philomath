a01c4465db42b0f31aeb668b545ba3f86ca13ab2
perhuepenbecker <perhuepenbecker@googlemail.com>
2024-11-18 23:51:43 +0100

    initial work on linreg.h/.c

--- source/linreg.c
+++ source/linreg.c
@@ -3,3 +3,28 @@
 //
 
 #include "linreg.h"
+
+void linear_regression_t_init(linear_regression_t* linreg, size_t dimensions, size_t epochs, double learning_rate){
+    linreg->bias = 0.0;
+    linreg->weights = calloc(dimensions, sizeof(double));
+    linreg->learning_rate = learning_rate;
+    linreg->mse = 0.0;
+    linreg->standardize = false;
+    linreg->x_means = NULL;
+    linreg->x_stds = NULL;
+    linreg->y_mean = 0.0;
+    linreg->y_std = 0.0;
+    linreg->dimensions = dimensions;
+    linreg->epochs = epochs;
+    linreg->data_points_count = 0;
+}
+
+void linear_regression_t_destroy(linear_regression_t* linreg){
+    free(linreg->weights);
+    if(linreg->x_means){
+        free(linreg->x_means);
+    }
+    if (linreg->x_stds){
+        free(linreg->x_stds);
+    }
+}
--- source/linreg.h
+++ source/linreg.h
@@ -2,7 +2,42 @@
 // Created by Per Hüpenbecker on 10.11.24.
 //
 
-#ifndef LELINEARREGRESSION_C_LINREG_H
-#define LELINEARREGRESSION_C_LINREG_H
+#ifndef LINEARREGRESSION_C_LINREG_H
+#define LINEARREGRESSION_C_LINREG_H
+
+#include <stddef.h>
+#include <stdbool.h>
+#include "preprocessor.h"
+#include "common.h"
+
+typedef struct {
+    double bias;
+    double* weights;
+    double learning_rate;
+    double mse;
+    bool standardize;
+
+    double* x_means;
+    double* x_stds;
+    double y_mean;
+    double y_std;
+
+    size_t dimensions;
+    size_t epochs;
+    size_t data_points_count;
+} linear_regression_t;
+
+void linear_regression_t_init(linear_regression_t* linreg, size_t dimensions, size_t epochs, double learning_rate);
+void linear_regression_t_destroy(linear_regression_t* linreg);
+
+void linear_regression_t_fit(linear_regression_t* linreg, dataset_t* dataset);
+void linear_regression_t_predict(linear_regression_t* linreg, dataset_t* dataset, double* x, double* y);
+
+void linear_regression_t_inspect(linear_regression_t* linreg);
+
+
+
+
 
 #endif //LINEARREGRESSION_C_LINREG_H
+
