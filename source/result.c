d6b30bd436c81cb2c1e6469d163b2ff2d466b6f3
perhuepenbecker <perhuepenbecker@googlemail.com>
2024-11-22 16:58:50 +0100

    Documentation - test commit after rename

--- source/result.c
+++ source/result.c
@@ -4,9 +4,13 @@
 
 #include "result.h"
 
+// Helper function to return a valid result
+
 Result Ok(void* data){
     return (Result) {.is_ok = true, .data=data};
 }
+
+// Helper function to return an error result
 
 Result Err(ErrorCode code, const char* message, const char* context) {
     return (Result){
@@ -15,12 +19,16 @@ Result Err(ErrorCode code, const char* message, const char* context) {
     };
 }
 
+// Helper function to perform error propagation
+
 Result Err_from(Error err){
     return (Result){
         .is_ok = false,
         .error = {.code = err.code, .message = err.message, .context = err.context}
     };
 }
+
+// Helper function to extract an error out of a result
 
 Error get_error(Result result){
     return result.error;
